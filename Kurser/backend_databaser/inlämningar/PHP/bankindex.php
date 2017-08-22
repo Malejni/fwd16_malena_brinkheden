5dbe1d870e526781945afc8d2b5d5c518ff3b77e
Malejni <malenabrinkheden@gmail.com>
2017-08-22 12:33:49 +0200

    added files

--- Kurser/backend_databaser/inlämningar/PHP/bankindex.php
+++ Kurser/backend_databaser/inlämningar/PHP/bankindex.php
@@ -6,21 +6,39 @@
     </head>
     <body>
         <?php
+        
+            include_once ('config.php');
         // Call documents needed to run the program
-        require (“config.php”);
         
        // Call store procedure and inputs an in parameter
-        $sql = “CALL Account(‘Sohail’)“;
+        $sql = "CALL Account(‘Sohail’)";
         $result = $conn->query($sql);
         
        // Fetches all rows with rowCount
         if ($result->rowCount()) {
-            $result->setFetchMode(PDO::FETCH_CLASS, “bank”);
+            $result->setFetchMode(PDO::FETCH_CLASS, "bank");
             // Prints the Firstname variable from the bank class
             while($row = $result->fetch()) {
                 echo $row->Firstname;
             }
         }
+        
+        class bank {
+    //Class variables
+    public $accountFirstname;
+    public $accountLastname;
+    public $accountName;
+    private $accountBalance;
+    public $Firstname;
+    
+   // Constructor who manages instances using data retrieved from DB
+    public function __construct() {
+        // Convert Float Value to an Integer
+        $this->accountBalance = round($this->accountBalance);
+        // Firstname gathers all necessary data from db and puts in a common variable
+        $this->Firstname = “{$this->accountFirstname} {$this->accountLastname} has: {$this->accountName} with {$this->accountBalance} balance”;
+    }
+}
         ?>
     </body>
 </html>
--- Kurser/backend_databaser/inlämningar/PHP/config.php
+++ Kurser/backend_databaser/inlämningar/PHP/config.php
@@ -1,39 +1,20 @@
 <!-- config.php -->
 
 <?php
-$dbname = ‘u1164707_Malena’;
-$hostname = ‘83.168.227.23’;
-$username = ‘db1164707_Malena’;
-$password = ‘pC3c)rw2Qi’;
-$options  = array(PDO::MYSQL_ATTR_INIT_COMMAND => “SET NAMES ‘UTF8’“);
-
 try {
-    $conn = new PDO(“mysql:host=$hostname;dbname=$dbname;“, $username,
-    $password, $options);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    /*** echo a message saying we have connected ***/
-    //echo ‘Connected to database.<br />’;
-}
-catch(PDOException $e){
-    // For debug purpose, shows all connection details
-    echo ‘Connection failed: ’.$e->getMessage().“<br/>“;
-      // Hide connection details.
-    //echo ‘Could not connect to database.<br />’); 
-}
-class bank {
-    //Class variables
-    public $accountFirstname;
-    public $accountLastname;
-    public $accountName;
-    private $accountBalance;
-    public $Firstname;
     
-   // Constructor who manages instances using data retrieved from DB
-    public function __construct() {
-        // Convert Float Value to an Integer
-        $this->accountBalance = round($this->accountBalance);
-        // Firstname gathers all necessary data from db and puts in a common variable
-        $this->Firstname = “{$this->accountFirstname} {$this->accountLastname} has: {$this->accountName} with {$this->accountBalance} balance”;
-    }
+   $conn = new PDO('mysql:host=83.168.227.23; dbname=db1164707_MalenaB', 'u1164707_MalenaB', 'pC3c)rw2Qi');
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    
+   echo $e->getMessage();
+    
+   die();
+    
 }
+
+
+
+
+
 ?>
